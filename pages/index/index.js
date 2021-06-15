cd0f7687ac62eeb49b9df837efee12adb71741be
liaolonghui <1340881930@qq.com>
2021-06-15 20:22:15 +0800

    轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,14 +4,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    // 轮播图数据
+    swiperList: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.getSwiperList()
+  },
+
+  getSwiperList() {
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result) => {
+        this.setData({
+          swiperList: result.data.message
+        })
+      }
+    })
   },
 
   /**
